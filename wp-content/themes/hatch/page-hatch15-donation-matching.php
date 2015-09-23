4da29e205f5baca4cc11689442fc97618c6d427e
John Yoder <johngilesyoder@gmail.com>
2015-09-23 16:49:17 -0600

    Put code in page template

--- wp-content/themes/hatch/page-hatch15-donation-matching.php
+++ wp-content/themes/hatch/page-hatch15-donation-matching.php
@@ -45,6 +45,38 @@
 
 							<?php the_content(); ?>
 
+							<div class="alert alert-matching">
+							  <img src="http://hatchexperience.org/wp-content/uploads/2015/09/dawn-matching.png" alt="goodworks-matching" />
+							  <p><strong>Dawn McGee &mdash; Goodworks Ventures</strong></p>
+							  <p>Dawn McGee is the new CEO of Good Works Ventures, LLC &ndash; a Montana based company that invests in high social impact companies. They consider companies that are paradigm changing and have the potential for good financial return. Good Works invests in early and mid stage companies that show promise in their technology, scalability and management.</p>
+							</div>
+
+							<div class="total-raised">
+							  <span>Fundraising goal: <strong>$10,000</strong></span><span>Total raised: <strong>$6,974</strong></span>
+							</div>
+
+							<div class="matching-progress progress-1">
+							  <span class="matching-progress-final">Contributed</span>
+							  <div class="progress">
+							    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
+							      $3,487
+							    </div>
+							  </div>
+							</div>
+
+							<div class="matching-progress progress-2">
+							  <span class="matching-progress-final">Matched</span>
+							  <div class="progress">
+							    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
+							      $3,487
+							    </div>
+							  </div>
+							</div>
+
+							<hr />
+
+							<?php echo do_shortcode('[give_form id="1162"]'); ?>
+
 						</article>
 						<!-- /article -->
 
