9c1ddf93152d7e5b5a6bd0ba7b377d4ded6f5605
John Yoder <johngilesyoder@gmail.com>
2015-12-23 00:44:03 -0700

    Update gravity form id

--- wp-content/themes/hatch/template-panama-registration.php
+++ wp-content/themes/hatch/template-panama-registration.php
@@ -16,7 +16,7 @@
 
       <?php endif; ?>
       
-      <?php gravity_form(8, false, false, false, '', true); ?>
+      <?php gravity_form(11, false, false, false, '', true); ?>
     </div>
   </div>
 
