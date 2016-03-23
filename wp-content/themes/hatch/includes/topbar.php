fc689c44768e8d865bb24a3f4797fc4a1daf8820
John Yoder <johngilesyoder@gmail.com>
2016-03-22 14:22:27 -1000

    Replace topbar content

--- wp-content/themes/hatch/includes/topbar.php
+++ wp-content/themes/hatch/includes/topbar.php
@@ -1,14 +1,14 @@
-<!-- <div class="topbar topbar-apply">
+<div class="topbar topbar-apply">
   <div class="container">
-    <p><strong>Hatch 12 (2015):</strong> Apply now to attend.<span> Guest list announcements will be made August 1, 2015.</span></p>
+    <p><strong>Hatch 2016:</strong> Apply now to attend.<span> Guest list announcements will be made August 1, 2016.</span></p>
     <a href="/apply" class="btn btn-secondary btn-apply">Apply Now</a> 
     <a href="https://filmfreeway.com/festival/HATCHExperience" class="btn btn-secondary btn-filmfreeway">Submit with <img src="<?php echo get_template_directory_uri(); ?>/assets/img/btn-filmfreeway.svg"></a>
   </div>
-</div> -->
-<div class="topbar topbar-support">
+</div>
+<!-- <div class="topbar topbar-support">
   <div class="container">
     <p><strong>Support HATCH:</strong> Students of today are our leaders of tomorrow. <span>It’s in the best interest of our future to invest in them.</span></p>
     <a href="/partners/support-contribute/" class="btn btn-secondary btn-support">Support Now</a> 
 
   </div>
-</div>
+</div> -->
