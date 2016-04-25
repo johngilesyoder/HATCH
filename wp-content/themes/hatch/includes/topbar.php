89ebe98ea79eb447d48d4062c8d52489e3549b91
John Yoder <johngilesyoder@gmail.com>
2016-04-25 12:58:54 -0600

    Revise topbar

--- wp-content/themes/hatch/includes/topbar.php
+++ wp-content/themes/hatch/includes/topbar.php
@@ -1,7 +1,8 @@
 <div class="topbar topbar-apply">
   <div class="container">
-    <p><strong>Hatch 2016:</strong> Apply now to attend.<span> Guest list announcements will be made August 1, 2016.</span></p>
-    <a href="/apply" class="btn btn-secondary btn-apply">Apply Now</a> 
+    <p><strong>Hatch 2016:</strong> Apply now to attend or volunteer.</p>
+    <a href="/apply" class="btn btn-secondary btn-apply">Apply Now</a>
+    <a href="/volunteer" class="btn btn-secondary btn-apply">Volunteer</a>
     <a href="https://filmfreeway.com/festival/HATCHExperience" class="btn btn-secondary btn-filmfreeway">Submit with <img src="<?php echo get_template_directory_uri(); ?>/assets/img/btn-filmfreeway.svg"></a>
   </div>
 </div>
