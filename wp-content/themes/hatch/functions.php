5f35d8f375a00e8ba2aa74dd74c28bef34c12cec
John Yoder <johngilesyoder@gmail.com>
2016-07-07 10:41:38 -0600

    Update thumbnail sizes

--- wp-content/themes/hatch/functions.php
+++ wp-content/themes/hatch/functions.php
@@ -79,7 +79,7 @@ if (function_exists('add_theme_support'))
     add_image_size('large', 700, '', true); // Large Thumbnail
     add_image_size('medium', 250, '', true); // Medium Thumbnail
     add_image_size('small', 120, '', true); // Small Thumbnail
-    add_image_size('medium-thumb', 768, '9999', true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+    add_image_size('medium-thumb', 768, '9999'); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use
     /*add_theme_support('custom-background', array(
