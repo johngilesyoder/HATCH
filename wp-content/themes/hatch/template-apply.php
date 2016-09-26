b48f927a454a462d94afffa412dfb572f6e4b3db
John Yoder <johngilesyoder@gmail.com>
2016-09-26 13:55:50 -0600

    Add custom field for apply form ID

--- wp-content/themes/hatch/template-apply.php
+++ wp-content/themes/hatch/template-apply.php
@@ -50,8 +50,8 @@
 									<!-- <div class="alert alert-warning">
 										The application window for HATCH 2015 has now closed. Check back here next year for HATCH 2016.
 									</div> -->
-									<?php $form_id = echo(types_render_field( 'field-slug', array() )); ?>
-									<?php gravity_form($form_id, false, false, false, '', true); ?>
+									<?php $form_id = types_render_field( 'application-form-id', array() ); ?>
+									<?php gravity_form(echo $form_id, false, false, false, '', true); ?>
 								</div>
 							</div>
 
