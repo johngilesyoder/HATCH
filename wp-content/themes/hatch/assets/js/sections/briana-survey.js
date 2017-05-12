f1f951df7fd05b5abc6e00a180cd6d92fd995e69
John Yoder <johngilesyoder@gmail.com>
2017-05-12 10:41:09 -0600

    Update form id

--- wp-content/themes/hatch/assets/js/sections/briana-survey.js
+++ wp-content/themes/hatch/assets/js/sections/briana-survey.js
@@ -5,12 +5,12 @@
     'use strict';
 
     // DOM ready, take it away
-    var gravityForm = $('#gform_wrapper_7');
+    var gravityForm = $('#gform_wrapper_28');
 
     $('input[type="range"]').rangeslider();
 
     $(document).on('input','input[data-input-id]', function() {
-      $('#input_7_' + $(this).data('input-id')).val($(this).val());
+      $('#input_28_' + $(this).data('input-id')).val($(this).val());
       $(this).siblings('output').val($(this).val());
       if ($(this).val() === '0') {
         $(this).siblings('output').html('None');
@@ -19,17 +19,17 @@
 
     $( "#input-first-name" ).keyup(function() {
       var value = $( this ).val();
-      $( "#input_7_195_3" ).attr('value', value );
+      $( "#input_28_195_3" ).attr('value', value );
     })
     .keyup();
     $( "#input-last-name" ).keyup(function() {
       var value = $( this ).val();
-      $( "#input_7_195_6" ).attr('value', value );
+      $( "#input_28_195_6" ).attr('value', value );
     })
     .keyup();
     $( "#input-email" ).keyup(function() {
       var value = $( this ).val();
-      $( "#input_7_196" ).attr('value', value );
+      $( "#input_28_196" ).attr('value', value );
     })
     .keyup();
 
