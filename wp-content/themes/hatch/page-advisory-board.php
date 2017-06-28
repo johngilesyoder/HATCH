bbce5b01e31928306ae02b1a4e6a539d556e1a8a
John Yoder <johngilesyoder@gmail.com>
2017-06-28 12:13:28 -0600

    style advisory / exec board page

--- wp-content/themes/hatch/page-advisory-board.php
+++ wp-content/themes/hatch/page-advisory-board.php
@@ -1,4 +1,32 @@
-<?php get_header(); ?>
+<?php get_header();
+
+$query_one_args = array(
+	'post_type' => 'board-member',
+	'posts_per_page' => '-1',
+	'meta_query' => array(
+		array(
+			'key' => 'exec_board',
+			'compare' => '==',
+			'value' => '1'
+		)
+	)
+);
+$query_one = new WP_Query( $query_one_args );
+
+$query_two_args = array(
+	'post_type' => 'board-member',
+	'posts_per_page' => '-1',
+	'meta_query' => array(
+		array(
+			'key' => 'exec_board',
+			'compare' => '==',
+			'value' => '0'
+		)
+	)
+);
+$query_two = new WP_Query( $query_two_args );
+
+?>
 
 	<main role="main">
 		<div class="container">
@@ -17,18 +45,17 @@
 					<!-- article -->
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 						<?php the_content(); ?>
-						
-						<div class="advisory-board-posts">
-							
-							<?php	query_posts( 'post_type=board-member&posts_per_page=-1' ); ?>
-							<?php 
+
+						<div class="advisory-board-posts exec-board">
+
+							<h4>Executive Board</h4>
+							<?php if ( $query_one->have_posts() ) : while ( $query_one->have_posts() ) : $query_one->the_post(); ?>
+							<?php
 								// featured image
 								$thumb_id = get_post_thumbnail_id();
 								$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'medium', true);
 								$thumb_url = $thumb_url_array[0];
 							?>
-
-							<?php while ( have_posts() ) : the_post(); ?>
 								<div class="board-member">
 							    <div class="row">
 							    	<div class="col-sm-3">
@@ -40,7 +67,37 @@
 								    </div>
 								  </div>
 							  </div>
-							<?php endwhile; ?>
+							<?php endwhile; else : ?>
+							<?php endif; ?>
+
+							<?php wp_reset_query(); ?>
+
+						</div>
+
+
+						<div class="advisory-board-posts">
+
+							<h4>Advisory Board</h4>
+							<?php if ( $query_two->have_posts() ) : while ( $query_two->have_posts() ) : $query_two->the_post(); ?>
+							<?php
+								// featured image
+								$thumb_id = get_post_thumbnail_id();
+								$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'medium', true);
+								$thumb_url = $thumb_url_array[0];
+							?>
+								<div class="board-member">
+							    <div class="row">
+							    	<div class="col-sm-3">
+							    		<?php the_post_thumbnail(); ?>
+							    	</div>
+							    	<div class="col-sm-9">
+								    	<h3 class="board-member-name"><?php the_title(); ?></h3>
+								    	<?php the_content(); ?>
+								    </div>
+								  </div>
+							  </div>
+							<?php endwhile; else : ?>
+							<?php endif; ?>
 
 							<?php wp_reset_query(); ?>
 
@@ -74,7 +131,7 @@
 					</div>
 
 					<?php else: ?>
-					
+
 					<?php endif; ?>
 				</div>
 			</div>
