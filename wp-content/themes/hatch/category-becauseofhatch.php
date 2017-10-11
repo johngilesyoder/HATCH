ddbfb13159685b555cc211df30b03310b9ac0a19
John Yoder <johngilesyoder@gmail.com>
2017-10-11 14:41:35 -0600

    Update becauseofhatch category summary

--- wp-content/themes/hatch/category-becauseofhatch.php
+++ wp-content/themes/hatch/category-becauseofhatch.php
@@ -5,11 +5,7 @@
     <main role="main">
       <div class="because-title">
         <strong>#BecauseOfHatch</strong>
-        <p>You were invited to HATCH — 100 at a time — because you are amazing. As you inspire others, you are inspired.</p>
-        <p>HATCH is a network in which we mutually invest in one another. Few things promote this movement of good more effectively than a collective and shared social tissue, showcasing the work we are doing throughout the world, under one hash tag.</p>
-        <p>#becauseOfHATCH brings all of our work collectively full circle. Use it.</p>
-        <p>Let's start a collective movement.  Attach #becauseOfHATCH to your projects, ideas, creations, speaking engagements, etc. By attaching the #becauseofHATCH hashtag to social media posts, packaging, conferences and causes, it creates a trackable MOVEMENT, and serves as a metric for securing future partners and funding.</p>
-        <p>Used consistently, #becauseOfHATCH not only promotes YOU, via HATCH, but serves as a trackable node in which each of you are supporting one other and the global HATCH community.</p>
+        <p>HATCH is a network of world-shapers that mentor and mutually invest in one another. As positive things happen #becauseOfHATCH, the tag is used to help track the many projects and initiatives that grow from HATCH to create a global movement for good.</p>
       </div>
       <?php get_template_part('loop'); ?>
 
