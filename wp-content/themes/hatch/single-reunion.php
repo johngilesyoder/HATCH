02fcda46aff3b37fddbbae8370b15f38f80e9505
John Yoder <johngilesyoder@gmail.com>
2017-11-13 13:34:34 -0600

    Add featured image template

--- wp-content/themes/hatch/single-reunion.php
+++ wp-content/themes/hatch/single-reunion.php
@@ -36,7 +36,11 @@ $summaryPhoto = get_field('summary_photo');
 			<div class="container">
 				<div class="row">
 					<div class="col-md-8">
-						<?php the_content(); ?>
+						<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+							<?php the_content(); ?>
+						<?php endwhile; ?>
+						<?php else: ?>
+						<?php endif; ?>
 					</div>
 					<div class="col-md-4">
 						<?php if( !empty($summaryPhoto) ): ?>
