7e23f81cf6c150ea9c46abf1ac9e8fd2e536af36
John Yoder <johngilesyoder@gmail.com>
2019-02-07 16:41:30 -0600

    Add 2019 Mexico attendees page

--- wp-content/themes/hatch/functions.php
+++ wp-content/themes/hatch/functions.php
@@ -258,7 +258,7 @@ function html5blank_styles()
 
     //wp_register_style('styles', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime(), 'all');
     //wp_enqueue_style('styles'); // Enqueue it!
-    wp_enqueue_style('styles', get_template_directory_uri() . '/assets/css/style.css', array(), '1.3', 'all');
+    wp_enqueue_style('styles', get_template_directory_uri() . '/assets/css/style.css', array(), '1.5', 'all');
 }
 
 // Load HTML5 Blank conditional styles
