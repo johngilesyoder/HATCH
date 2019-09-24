ca84d99874cf2c55c132365b3ad10e77d80a9a3a
John Yoder <johngilesyoder@gmail.com>
2019-09-24 14:20:29 -0600

    Update price on registration page

--- wp-content/themes/hatch/template-registration.php
+++ wp-content/themes/hatch/template-registration.php
@@ -42,7 +42,7 @@ $photo_gallery_id = get_field('photo_gallery_id');
         <div class="col-md-12">
           <div class="welcome-section">
             <h4>Welcome to the HATCH network</h4>
-            <p>HATCH is a non-profit designed to benefit you and expand our network to HATCH a Better World, as well as mentor the next generation of our global leaders.  It costs $2500 to host each individual guest. HATCH is all inclusive.  Once you get yourself to Montana, everything is taken care of:  lodging, food, workshops, speaker series, entertainment, music, and transportation to and from the airport are all included with your pass.</p>
+            <p>HATCH is a non-profit designed to benefit you and expand our network to HATCH a Better World, as well as mentor the next generation of our global leaders.  It costs $2750 to host each individual guest. HATCH is all inclusive.  Once you get yourself to Montana, everything is taken care of:  lodging, food, workshops, speaker series, entertainment, music, and transportation to and from the airport are all included with your pass.</p>
             <p>We do not want financial constraints to stand in the way of your ability to experience HATCH. If the cost is prohibitive, there is an option to apply for a sponsored cost reduction when registering.</p>
           </div>
         </div>
