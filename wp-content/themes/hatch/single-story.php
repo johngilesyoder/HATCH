21910ee280889020bf37279be7de5cbacbc290a4
John Yoder <johngilesyoder@gmail.com>
2019-10-07 14:35:38 -0600

    Move video down and make all story sections conditional

--- wp-content/themes/hatch/single-story.php
+++ wp-content/themes/hatch/single-story.php
@@ -11,6 +11,7 @@
   $categories = get_the_term_list( $post->ID, 'story_category', '<li>', '</li><li>', '</li>' );
   $categories = strip_tags( $categories, '<li>' );
   $sdgs = get_the_terms( $post->ID, 'sdg' );
+  $the_content = apply_filters('the_content', get_the_content());
 ?>
 
 	<main role="main" aria-label="Content">
@@ -32,91 +33,97 @@
                 <span class="project-name"><?php the_title(); ?></span>
               </div>
             </header>
-            <?php if ($video) : ?>
-              <div class="lab-section project-video">
-                <div class="iframe-container">
-                  <?php echo $video; ?>
-                </div>
-              </div>
-            <?php endif; ?>
-            <div class="project-leader lab-section">
-              <label>Project Leader</label>
-              <div class="leader">
-                <img class="leader-avatar" src="<?php echo $leader_avatar['url']; ?>">
-                <div class="leader-details">
-                  <span class="leader-name"><?php echo $leader_name; ?></span>
-                  <div class="leader-bio">
-                    <?php echo $leader_bio; ?>
+            <?php if ($leader_name && $leader_avatar && $leader_bio) : ?>
+              <div class="project-leader lab-section">
+                <label>Project Leader</label>
+                <div class="leader">
+                  <img class="leader-avatar" src="<?php echo $leader_avatar['url']; ?>">
+                  <div class="leader-details">
+                    <span class="leader-name"><?php echo $leader_name; ?></span>
+                    <div class="leader-bio">
+                      <?php echo $leader_bio; ?>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            <?php endif; ?>
             <div class="classifications">
-              <div class="categories lab-section">
-                <label>Categories</label>
-                <ul>
-                  <?php echo $categories; ?>
-                </ul>
-              </div>
-              <div class="sdgs lab-section">
-                <label>SDGs <small>(UN Sustainable Development Goals)</small></label>
-                <div class="sdgs">
-                  <?php foreach ($sdgs as $sdg) : ?>
-                    <?php $sdg_tiles = apply_filters( 'taxonomy-images-get-terms', '', array(
-                        'taxonomy' => 'sdg',
-                          'term_args' => array(
-                            'slug' => $sdg->slug,
-                          )
-                        ) 
-                      );
-                    ?>
-                    <?php foreach( (array) $sdg_tiles as $sdg_tile) : ?>
-                      <div class="sdg">
-                        <?php echo wp_get_attachment_image( $sdg_tile->image_id, 'full', array('class' => 'sdg-image')); ?>
-                      </div>
-                    <?php endforeach; ?>
-                  <?php endforeach; ?>
+              <?php if (! empty( $categories )) : ?>
+                <div class="categories lab-section">
+                  <label>Categories</label>
+                  <ul>
+                    <?php echo $categories; ?>
+                  </ul>
                 </div>
+              <?php endif; ?>
+              <?php if (! empty( $sdgs )) : ?>
+                <div class="sdgs lab-section">
+                  <label>SDGs <small>(UN Sustainable Development Goals)</small></label>
+                  <div class="sdgs">
+                    <?php foreach ($sdgs as $sdg) : ?>
+                      <?php $sdg_tiles = apply_filters( 'taxonomy-images-get-terms', '', array(
+                          'taxonomy' => 'sdg',
+                            'term_args' => array(
+                              'slug' => $sdg->slug,
+                            )
+                          ) 
+                        );
+                      ?>
+                      <?php foreach( (array) $sdg_tiles as $sdg_tile) : ?>
+                        <div class="sdg">
+                          <?php echo wp_get_attachment_image( $sdg_tile->image_id, 'full', array('class' => 'sdg-image')); ?>
+                        </div>
+                      <?php endforeach; ?>
+                    <?php endforeach; ?>
+                  </div>
+                </div>
+              <?php endif; ?>
+            </div>
+            <?php if (!empty($the_content)) : ?>
+              <div class="summary lab-section">
+                <label>Summary</label>
+                <div><?php the_content(); ?></div>
               </div>
-            </div>
-            <div class="summary lab-section">
-              <label>Summary</label>
-              <div><?php the_content(); ?></div>
-            </div>
+            <?php endif; ?>
+            <?php if ($project_description) : ?>
             <div class="project-description lab-section">
               <label>Project Description</label>
               <div>
                 <?php echo $project_description; ?>
               </div>
             </div>
-            <div class="project-needs lab-section">
-              <label>Project Needs</label>
-              <div><?php echo $needs; ?></div>
-            </div>
+            <?php endif; ?>
+            <?php if ($needs) : ?>
+              <div class="project-needs lab-section">
+                <label>Project Needs</label>
+                <div><?php echo $needs; ?></div>
+              </div>
+            <?php endif; ?>
             <div class="actions">
               <div class="links lab-section">
                 <label>Links</label>
                 <?php if( have_rows('links') ): ?>
+                  
+                  <ul>
+                  <?php while( have_rows('links') ): the_row(); 
 
-                <?php while( have_rows('links') ): the_row(); 
+                    // vars
+                    $link_label = get_sub_field('link_label');
+                    $link_url = get_sub_field('link_url')
 
-                  // vars
-                  $link_label = get_sub_field('link_label');
-                  $link_url = get_sub_field('link_url')
+                    ?>
 
-                  ?>
+                    <li>
 
-                  <li>
+                      <?php if( $link_label ): ?>
+                        <a href="<?php echo $link_url; ?>"><?php echo $link_label; ?></a>
+                      <?php endif; ?>
 
-                    <?php if( $link_label ): ?>
-                      <a href="<?php echo $link_url; ?>"><?php echo $link_label; ?></a>
-                    <?php endif; ?>
+                    </li>
 
-                  </li>
+                  <?php endwhile; ?>
 
-                <?php endwhile; ?>
-
-                </ul>
+                  </ul>
 
                 <?php endif; ?>
               </div>
@@ -126,7 +133,7 @@
                   <div><?php echo $donate; ?></div>
                 </div>
               <?php endif; ?>
-              <?php if ($donate) : ?>
+              <?php if ($contact) : ?>
                 <div class="contact lab-section">
                   <label>Contact</label>
                   <div><?php echo $contact; ?></div>
@@ -139,30 +146,35 @@
                 <div class="lab-section updates">
                   <label>Project Updates</label>
 
-                <?php while( have_rows('project_updates') ): the_row(); 
+                  <?php while( have_rows('project_updates') ): the_row(); 
 
-                  // vars
-                  $title = get_sub_field('update_title');
-                  $date = get_sub_field('update_date');
-                  $text = get_sub_field('update_text');
+                    // vars
+                    $title = get_sub_field('update_title');
+                    $date = get_sub_field('update_date');
+                    $text = get_sub_field('update_text');
 
-                  ?>
+                    ?>
 
-                  <div class="update">
-                    <div class="update-title"><?php echo $title; ?></div>
-                    <div class="update-date"><?php echo $date; ?></div>
-                    <div class="update-content"><?php echo $text; ?></div>
-                  </div>
+                    <div class="update">
+                      <div class="update-title"><?php echo $title; ?></div>
+                      <div class="update-date"><?php echo $date; ?></div>
+                      <div class="update-content"><?php echo $text; ?></div>
+                    </div>
 
-                <?php endwhile; ?>
+                  <?php endwhile; ?>
 
-              </div>
+                </div>
 
-            <?php endif; ?>
-
-
+              <?php endif; ?>
 
             </div>
+            <?php if ($video) : ?>
+              <div class="lab-section project-video">
+                <div class="iframe-container">
+                  <?php echo $video; ?>
+                </div>
+              </div>
+            <?php endif; ?>
           </div>
         </div>
       </div>
