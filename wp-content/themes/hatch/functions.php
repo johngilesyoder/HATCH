897db666621f785da336faf50bc52bafd78a1dbd
John Yoder <johngilesyoder@gmail.com>
2019-10-09 16:02:09 -0600

    Update challenge texts

--- wp-content/themes/hatch/functions.php
+++ wp-content/themes/hatch/functions.php
@@ -243,7 +243,7 @@ function html5blank_conditional_scripts()
 
     if ( is_page_template( 'template-labs-voting.php' ) ) {
 
-      wp_register_script('labs-voting', get_template_directory_uri() . '/assets/js/sections/labs-voting.js', array(), '1.0.0' );
+      wp_register_script('labs-voting', get_template_directory_uri() . '/assets/js/sections/labs-voting.js', array(), '1.0.1' );
       wp_enqueue_script('labs-voting'); // Enqueue it!
 
     }
