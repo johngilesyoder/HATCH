0afd9de2a56a1b50f7821def177a2906bd4db1a5
John Yoder <johngilesyoder@gmail.com>
2020-06-18 16:06:00 -0600

    Subscribe on GLR page

--- wp-content/themes/hatch/template-home.php
+++ wp-content/themes/hatch/template-home.php
@@ -6,6 +6,51 @@
   </section>
 
   <div class="container">
+    <section class="section-block">
+      <div class="section-header">
+        <h2 class="section-title">Subscribe to Global Living Room Invites &amp; Updates</h2>
+      </div>
+      <!-- Begin Mailchimp Signup Form -->
+      <div id="mc_embed_signup">
+        <form action="https://hatchexperience.us12.list-manage.com/subscribe/post?u=80e369f4604bb2b82d06aebf5&amp;id=bc2abe573d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll">
+            <div class="form-group">
+              <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+              <input type="email" value="" name="EMAIL" class="required form-control" id="mce-EMAIL">
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="mce-FNAME">First Name </label>
+                  <input type="text" value="" name="FNAME" class="form-control" id="mce-FNAME">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="mce-LNAME">Last Name </label>
+                  <input type="text" value="" name="LNAME" class="form-control" id="mce-LNAME">
+                </div>
+              </div>
+            </div>
+            <div id="mce-responses" class="clear">
+              <div class="response" id="mce-error-response" style="display:none"></div>
+              <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true">
+              <input type="text" name="b_80e369f4604bb2b82d06aebf5_bc2abe573d" tabindex="-1" value="">
+            </div>
+            <div class="clear">
+              <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">Subscribe</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='MMERGE5';ftypes[5]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+      <!--End mc_embed_signup-->
+    </section>
+  </div>
+
+  <div class="container">
     <!-- Conference -->
   	<section class="section-block section-conference">
       <div class="section-header">
--- wp-content/themes/hatch/template-living-room.php
+++ wp-content/themes/hatch/template-living-room.php
@@ -75,6 +75,59 @@ $supportSectionContent = get_field('support_section_content');
           <div class="row">
 
             <div class="col-md-12">
+              <!-- Begin Mailchimp Signup Form -->
+              <div id="mc_embed_signup">
+                <form action="https://hatchexperience.us12.list-manage.com/subscribe/post?u=80e369f4604bb2b82d06aebf5&amp;id=bc2abe573d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                  <div id="mc_embed_signup_scroll">
+                    <div class="form-group">
+                      <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+                      <input type="email" value="" name="EMAIL" class="required form-control" id="mce-EMAIL">
+                    </div>
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label for="mce-FNAME">First Name </label>
+                          <input type="text" value="" name="FNAME" class="form-control" id="mce-FNAME">
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label for="mce-LNAME">Last Name </label>
+                          <input type="text" value="" name="LNAME" class="form-control" id="mce-LNAME">
+                        </div>
+                      </div>
+                    </div>
+                    <div id="mce-responses" class="clear">
+                      <div class="response" id="mce-error-response" style="display:none"></div>
+                      <div class="response" id="mce-success-response" style="display:none"></div>
+                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                      <input type="text" name="b_80e369f4604bb2b82d06aebf5_bc2abe573d" tabindex="-1" value="">
+                    </div>
+                    <div class="clear">
+                      <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">Subscribe</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='MMERGE5';ftypes[5]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+              <!--End mc_embed_signup-->
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+        
+
+  <section class="living-room-section calendar">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="section-title"><?php echo $calendarSectionTitle; ?></h2>
+          <div class="row">
+
+            <div class="col-md-12">
               <a 
                 class="btn btn-secondary" 
                 href="https://calendar.google.com/calendar?cid=aGF0Y2hleHBlcmllbmNlLm9yZ19rNDRlajdzbjhxdGRhNDFwZW5nOHNsNXZuOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
