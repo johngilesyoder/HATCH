b400d3449a7810b7ee25f247836e52d29d437a0a
John Yoder <johngilesyoder@gmail.com>
2021-02-11 09:43:14 -1000

    Fix registration page styling

--- wp-content/themes/hatch/lib/styles-scripts.php
+++ wp-content/themes/hatch/lib/styles-scripts.php
@@ -87,7 +87,7 @@ function html5blank_conditional_scripts()
 // Load global styles
 function html5blank_styles() {
   if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
-    wp_register_style('styles', get_template_directory_uri() . '/dist/asset/css/bundle.css', array(), '1.5', 'all');
+    wp_register_style('styles', get_template_directory_uri() . '/dist/asset/css/bundle.css', array(), '1.5.1', 'all');
     wp_enqueue_style('styles'); // Enqueue it!
 
     wp_register_style('materialIcons', 'https://fonts.googleapis.com/icon?family=Material+Icons', array(), '1.0', 'all');
